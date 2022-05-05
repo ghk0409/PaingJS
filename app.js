5554d4963b83515f040d995f6a7d3a5e56ea6c3c
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2022-05-05 16:34:58 +0900

    캔버스 채우기 및 기타 기능 추가
    + 마우스 좌클릭 시에만 그리기 활성화
    + 캔버스 위에서 우클릭 시, 우클릭 방지(context menu prevent)

--- app.js
+++ app.js
@@ -6,13 +6,22 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const clear = document.getElementById("jsClear");
+const save = document.getElementById("jsSave");
+
+const INITAIL_COLOR = "#2c2c2c";
 
 // pixel manipulation 설정
 canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
 canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
 
+// canvas 초기 배경색 설정(흰색)
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 // 기본 canvas-context 속성 설정
-ctx.strokeStyle = "#2c2c2c"; // 라인 색상
+ctx.strokeStyle = INITAIL_COLOR; // 라인 색상
+ctx.fillStyle = INITAIL_COLOR; // 채우기 색상
 ctx.lineWidth = 2.5; // 라인 두꼐
 
 let painting = false;
@@ -23,9 +32,13 @@ function stopPainting() {
     painting = false;
 }
 
-// 페인팅 진행 함수
-function startPainting() {
-    painting = true;
+// 페인팅 진행 함수 - 마우스 왼쪽 클릭 시에만
+function startPainting(event) {
+    if (event.which == 1) {
+        painting = true;
+    } else {
+        alert("마우스 왼쪽 버튼으로 그려주세요!!");
+    }
 }
 
 // 마우스 움직일 시 함수
@@ -46,6 +59,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 // 선 굵기 변경 함수
@@ -55,7 +69,7 @@ function handleRangeChange(event) {
 }
 
 // 모드 변경 함수
-function handleModeClick(event) {
+function handleModeClick() {
     if (filling) {
         filling = false;
         mode.innerText = "fill";
@@ -63,6 +77,23 @@ function handleModeClick(event) {
         filling = true;
         mode.innerText = "paint";
     }
+}
+
+// 캔버스 채우기 함수
+function handleCanvasClick() {
+    if (filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+// 캔버스 지우기 함수
+function handleClearClick() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+// 캔버스 우클릭 방지 함수
+function handleRightClick(event) {
+    event.preventDefault();
 }
 
 if (canvas) {
@@ -74,6 +105,10 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     // 마우스가 canvas 벗어날 때 이벤트
     canvas.addEventListener("mouseleave", stopPainting);
+    // filling == tru일 경우, 클릭 시 채우기 이벤트
+    canvas.addEventListener("click", handleCanvasClick);
+    // 우클릭 방지 이벤트
+    canvas.addEventListener("contextmenu", handleRightClick);
 }
 
 Array.from(colors).forEach((clickedColor) =>
@@ -86,4 +121,12 @@ if (range) {
 
 if (mode) {
     mode.addEventListener("click", handleModeClick);
+}
+
+if (clear) {
+    clear.addEventListener("click", handleClearClick);
+}
+
+if (save) {
+    save.addEventListener("click", handleSaveClick);
 }
